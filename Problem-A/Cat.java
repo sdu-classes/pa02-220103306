b13df7716045ad62f9559114478b935a76068600
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-11 18:42:28 +0600

    Create Cat.java

---
+++ Problem-A/Cat.java
@@ -0,0 +1,20 @@
+package Animal;
+
+public class Cat extends Mammal{
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        super(name);
+    }
+    
+    public void greets(){
+        System.out.println("Meow");
+    }
+    
+    @Override
+    public String toString() {
+        return "Cat[" + super.toString() + ']';
+    }
+}
