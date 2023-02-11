099e8d1f71e7f663de51c17d76d5387e16215672
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-11 18:47:25 +0600

    Create Animal.java

---
+++ Problem-A/Animal.java
@@ -0,0 +1,18 @@
+class Animal {
+    private String name;
+
+    public Animal() {
+    }
+
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal[" + "name=" + name + ']';
+    }
+    
+    
+}
