2089f44e5be02d49a940e8be3be95a7a9ea154a1
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-11 17:22:03 +0600

    Create Square.java

---
+++ problem-B/Square.java
@@ -0,0 +1,40 @@
+public class Square extends Rectangle{
+
+    public Square(){}
+
+    public Square(double side){
+        super(side,side);
+    }
+
+    public Square(double side, String color,boolean filled){
+        super(side,side,color,filled);
+    }
+
+    public double getSide(){
+        return super.getLength();
+    }
+
+    public void setSide(double side){
+        this.setLength(side);
+        this.setWidth(side);
+    }
+
+    public void setWidth(double side){
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    public void setLength(double side){
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString(){
+        return "Square[" + super.toString() + "]";
+    }
+
+
+
+
+}
