aaee3e601d138ac0539e6195a95cc9ca96b1a8f4
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-11 18:42:52 +0600

    Create Dog.java

---
+++ Problem-A/Dog.java
@@ -0,0 +1,23 @@
+package Animal;
+public class Dog extends Mammal{
+
+    public Dog() {
+    }
+
+    public Dog(String name) {
+        super(name);
+    }
+    
+    public void greets(){
+        System.out.println("Woof");
+    }
+    
+    public void greets(Dog another){
+        System.out.println("Woooof");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog[" + super.toString() + ']';
+    }
+}
